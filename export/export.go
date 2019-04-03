770cf21bfdb5ad9e2319b63a1cff5857a3b2d3a1
Tristan Messner <tristanmessner@gmail.com>
2019-04-02 21:04:16 -0400

    cleaned up my commit

--- export/export.go
+++ export/export.go
@@ -1,32 +1,31 @@
 package export
 
 import (
-        "encoding/json"
-        "os"
+	"encoding/json"
+	"os"
 )
 
-func appendFile(filename string, data string) error{
-        file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0664)
-        if err != nil{
-                return err
-        }
-        _, err = file.WriteString(data+"\n")
-        if err != nil{
-                return err
-        }
-        defer file.Close()
-        return nil
+func appendFile(filename string, data string) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0664)
+	if err != nil {
+		return err
+	}
+	_, err = file.WriteString(data + "\n")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return nil
 }
 
-func ExportJson(data interface{}) error{
-        jsonstring, err := json.Marshal(data)
-        if err != nil{
-                return err
-        }
-        err = appendFile("logs/postprocessing.json",string(jsonstring))
-        if err != nil{
-                return err
-        }
-        return nil
+func ExportJson(data interface{}) error {
+	jsonstring, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	err = appendFile("logs/postprocessing.json", string(jsonstring))
+	if err != nil {
+		return err
+	}
+	return nil
 }
-
--- streamproc/streamproc.go
+++ streamproc/streamproc.go
@@ -4,12 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/RayofLightz/ingestion/enrich"
-    "github.com/RayofLightz/ingestion/export"
+	"github.com/RayofLightz/ingestion/export"
 	"github.com/RayofLightz/ingestion/extract"
 	"net"
 	"reflect"
+	"strconv"
 	"sync"
-    "strconv"
 )
 
 func listner(pipe chan string, conf map[string]bool) error {
@@ -47,32 +47,30 @@ func worker(wg *sync.WaitGroup, id int, pipe chan string, procp chan []map[strin
 	processData(pipe, procp)
 }
 
-
-
 func processData(pipe chan string, procp chan []map[string]string) {
-    var lockedret struct{
-            mux sync.Mutex
-            valueArray []map[string]string
-    }
+	var lockedret struct {
+		mux        sync.Mutex
+		valueArray []map[string]string
+	}
 	for {
 		pdata := <-pipe
 		data, err := extract.ExtractJsonRecord(pdata)
-        defer lockedret.mux.Lock()
-        lockedret.valueArray = recurs(data)
-        defer lockedret.mux.Unlock()
+		defer lockedret.mux.Lock()
+		lockedret.valueArray = recurs(data)
+		defer lockedret.mux.Unlock()
 		if err != nil {
 			fmt.Println("error in extraction")
 			fmt.Println(err)
 		} else {
-            defer lockedret.mux.Lock()
+			defer lockedret.mux.Lock()
 			procp <- lockedret.valueArray
-            defer lockedret.mux.Unlock()
+			defer lockedret.mux.Unlock()
 		}
 	}
 }
 
 func recurs(pipe_var map[string]interface{}) []map[string]string {
-    tmp_mp := make([]map[string]string,0,20)
+	tmp_mp := make([]map[string]string, 0, 20)
 	return_mp := make(map[string]string)
 	for index, _ := range pipe_var {
 		typ := reflect.TypeOf(pipe_var[index])
@@ -98,11 +96,11 @@ func recurs(pipe_var map[string]interface{}) []map[string]string {
 			tmpjson = pipe_var[index].(json.Number)
 			add_val := tmpjson.String()
 			return_mp[index] = add_val
-		}else if typ.String() == "bool"{
-            var tmpstr string
-            tmpstr = strconv.FormatBool(pipe_var[index].(bool))
-            return_mp[index] = tmpstr
-        } else if typ.String() == "map[string]interface {}" {
+		} else if typ.String() == "bool" {
+			var tmpstr string
+			tmpstr = strconv.FormatBool(pipe_var[index].(bool))
+			return_mp[index] = tmpstr
+		} else if typ.String() == "map[string]interface {}" {
 			mp := pipe_var[index].(map[string]interface{})
 			tmp := recurs(mp)
 			for indexb, _ := range tmp {
@@ -132,11 +130,11 @@ func calculate(pipe chan []map[string]string, conf map[string]bool) {
 				}
 			}
 		}
-        fmt.Println(pipe_var)
-        err := export.ExportJson(pipe_var)
-        if err != nil{
-                fmt.Println(err)
-        }
+		fmt.Println(pipe_var)
+		err := export.ExportJson(pipe_var)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
 
